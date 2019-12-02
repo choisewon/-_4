51abc33ef6507609e4fdd520b56ad228c82f9bdd
TakamichiNomura <hs19980101@yahoo.co.jp>
2019-12-02 13:31:26 +0900

    Mypanel

--- MyPanel.java
+++ MyPanel.java
@@ -10,23 +10,33 @@ import java.awt.event.*;
  */
 public class MyPanel extends JPanel
 {
-    JLabel mlName,mlDept,mlAddress;
+    JLabel mlName,mlDept,mlAddress,mlSchoolYear;
     JTextField tfName,tfDept,tfAddress;
+    JButton mb,mbClear;
+    JComboBox<String> cdSchoolYear;
+    TextArea ta;
     public MyPanel(){
         this.setLayout(new FlowLayout());
         mlName = new JLabel("이름");
         mlDept = new JLabel("학과");
         mlAddress = new JLabel("주소");
+        mlSchoolYear = new JLabel("학년");
         tfName = new JTextField(14);
-        tfDept = new JTextField("�?로벌?��?��?��?��?��",14);
+        tfDept = new JTextField("글로벌소프트웨어",14);
         tfAddress = new JTextField(14);
-        
+        String[] array = {"1학년","2학년","3학년","4학년"};
+        cdSchoolYear = new JComboBox<String>(array);
+        mb = new JButton("추가");
+        mbClear = new JButton("Clear");
         this.add(mlName);
         this.add(tfName);
         this.add(mlDept);
         this.add(tfDept);
         this.add(mlAddress);
         this.add(tfAddress);
-        
+        this.add(mlSchoolYear);
+        this.add(cdSchoolYear);
+        this.add(mb);
+        this.add(mbClear);
     }
 }
