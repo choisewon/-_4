068ac289ad5a2f8f600710b8f7afaf977f3dce98
choisewon <chltpdnjs5@naver.com>
2019-12-02 13:29:26 +0900

    프레임 메소드를 작성하였습니다.

--- TextFrame.java
+++ TextFrame.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 
 /**
  * 여기에 TextFrame 클래스 설명을 작성하십시오.
@@ -5,29 +6,20 @@
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
-public class TextFrame
+public class TextFrame extends JFrame
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * TextFrame 클래스의 객체 생성자
-     */
-    public TextFrame()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
-    }
+   private JPanel mp;
+   public TextFrame()
+   {
+       setTitle("실습_4");
+       setSize(250,350);
+       
+       mp = new JPanel();
+       
+       this.add(mp);
+       
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       setVisible(true);
+   }
 }
